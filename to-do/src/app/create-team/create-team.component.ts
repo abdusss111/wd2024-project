a5cf5cf10c49893b6cf0b404ab54310c9d31a7d5
deniskhan22bd <odinstrelok02@gmail.com>
2024-04-12 10:17:24 +0600

    half finish create team

--- to-do/src/app/create-team/create-team.component.ts
+++ to-do/src/app/create-team/create-team.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-team',
@@ -14,12 +14,26 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './create-team.component.css'
 })
 export class CreateTeamComponent {
-  items = [1, 2 ,3 ,4 ,5]
+  teamForm = this.formBuilder.group({
+    teamName : '',
+    teammates : this.formBuilder.array([this.formBuilder.control('')])
+  })
 
-  inputs: string[] = [""];
+  constructor(private formBuilder: FormBuilder) {}
 
-  teamName = new FormControl("")
-  addInput() {
-    this.inputs.push("");
+
+  // Return a teammates
+  get teammates(){
+    return this.teamForm.get('teammates') as FormArray;
+  }
+
+  // Add a new <input>
+  addTeammate(){
+    this.teammates.push(this.formBuilder.control(''));
+  }
+
+  // There will be a logic of using service with POST request
+  onSubmit(){
+    console.log(this.teamForm.value)
   }
 }
