e004c8da03d3d0b5787eaf24966ffedcb27a76c0
Alina Abdullina <fianit8@gmail.com>
2024-04-24 14:57:08 +0500

    add task calendar

--- to-do/src/app/models.ts
+++ to-do/src/app/models.ts
@@ -1,31 +1,30 @@
 export interface User {
-  value(value: any): string
-  username: string
-  id: number
-  name: string
-  lastname: string
-  password: string
-  email: string
-  isLeader: boolean
-  photoUrl: string
-  team: string
+  value(value: any): string;
+  username: string;
+  id: number;
+  name: string;
+  lastname: string;
+  password: string;
+  email: string;
+  isLeader: boolean;
+  photoUrl: string;
+  team: string;
 }
 
 export interface Team {
-  id?: number
-  name: string
+  id?: number;
+  name: string;
 }
 export interface Folder {
-  id: number
-  name: string
-  user_id: number
+  id: number;
+  name: string;
+  user_id: number;
 }
 
 export interface Token {
   access: string;
   refresh: string;
 }
-
 
 export interface Task {
   title: string;
